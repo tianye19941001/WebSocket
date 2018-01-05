b18c943231300f75ea9700536fc6c83f52d11491
原野 <yuanye@yuanyedeMacBook-Pro.local>
2018-01-05 14:35:36 +0800

    bug解决

--- demo4/WebSocket.js
+++ demo4/WebSocket.js
@@ -9,20 +9,20 @@ app.listen(PORT);
 
 io.on('connection', function (socket) {
   player.count++;
-  player.name = 'player' + player.count;
+  socket.nickName = 'player' + player.count;
 
-  io.emit('connects', player.name + '加入聊天室～');
+  io.emit('connects', socket.nickName + '加入聊天室～');
 
   socket.on('message', function (str) {
-    io.emit('message', player.name + ':' + str);
+    io.emit('message', socket.nickName + ':' + str);
   });
 
   socket.on('disconnect', function(str) {
-  	io.emit('disconnect', player.name + '离开了聊天室');
+  	io.emit('disconnect', socket.nickName + '离开了聊天室');
   });
 
   socket.on('changeName', function(str) {
-  	player.name = str;
+  	socket.nickName = str;
   	socket.emit('changeName', '名字已经换成：' + str );
   })
 });
